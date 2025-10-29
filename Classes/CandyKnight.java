f5c905a2b478f30612a17eaa55df269e79f7da87
FelipeVieira9 <felipe.vieiraa52@gmail.com>
2025-10-29 16:49:04 -0300

    Update

--- Classes/CandyKnight.java
+++ Classes/CandyKnight.java
@@ -14,19 +14,10 @@ public class CandyKnight {
         System.out.println("--- Combate iniciado! ---");
         System.out.println(jogador.getNome() + " vs " + monstro.getNome());
 
-        // Loop de combate
         while (jogador.estaVivo() && monstro.estaVivo()) {
-            
-            // 1. O jogador ataca o monstro
-            // POLIMORFISMO: Não importa qual monstro é,
-            // o cavaleiro simplesmente chama "atacar".
             jogador.atacar(monstro);
 
             if (monstro.estaVivo()) {
-                // 2. O monstro ataca o jogador
-                // POLIMORFISMO: Não importa se é um UrsoDeGoma ou SoldadoGengibre,
-                // o jogo simplesmente diz "monstro, ataque!".
-                // O Java chama o @Override de atacar() correto.
                 monstro.atacar(jogador);
             }
         }
@@ -38,20 +29,15 @@ public class CandyKnight {
         }
     }
 
-    // Método principal para testar
     public static void main(String[] args) {
         CandyKnight meuJogo = new CandyKnight();
         Cavaleiro heroi = new Cavaleiro("Sir Crocante");
         
-        // Polimorfismo na prática:
         MonstroDoce monstro1 = new UrsoDeGoma();
         MonstroDoce monstro2 = new SoldadoGengibre();
 
-        // O método iniciarCombate() aceita qualquer coisa
-        // que "É UM" MonstroDoce.
         meuJogo.iniciarCombate(heroi, monstro1);
         
-        // Se o herói ainda estiver vivo
         if(heroi.estaVivo()) {
              meuJogo.iniciarCombate(heroi, monstro2);
         }
