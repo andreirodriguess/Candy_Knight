ee745bb5ec26440b0b24db735a36b8f6208424ed
Lucas Figueiredo <lucasricardolf11@gmail.com>
2025-11-03 15:39:21 -0300

    Main code - Candy Kngiht
    inicia o jogo na main

--- Classes/CandyKnight.java
+++ Classes/CandyKnight.java
@@ -1,45 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.mycompany.candyknight;
+package candyknight;
 
-/**
- *
- * @author felip
- */
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class CandyKnight {
 
-    public void iniciarCombate(Cavaleiro jogador, MonstroDoce monstro) {
-        System.out.println("--- Combate iniciado! ---");
-        System.out.println(jogador.getNome() + " vs " + monstro.getNome());
-
-        while (jogador.estaVivo() && monstro.estaVivo()) {
-            jogador.atacar(monstro);
-
-            if (monstro.estaVivo()) {
-                monstro.atacar(jogador);
-            }
-        }
-
-        // Fim do combate
-        if (!monstro.estaVivo()) {
-            System.out.println(jogador.getNome() + " venceu!");
-            jogador.coletarDinheiro(monstro.getRecompensaEmDinheiro());
-        }
-    }
-
     public static void main(String[] args) {
-        CandyKnight meuJogo = new CandyKnight();
-        Cavaleiro heroi = new Cavaleiro("Sir Crocante");
         
-        MonstroDoce monstro1 = new UrsoDeGoma();
-        MonstroDoce monstro2 = new SoldadoGengibre();
+         ControleJanela game = new ControleJanela();
+         
+         game.start();
+         
+    };
 
-        meuJogo.iniciarCombate(heroi, monstro1);
-        
-        if(heroi.estaVivo()) {
-             meuJogo.iniciarCombate(heroi, monstro2);
-        }
-    }
 }
+    
