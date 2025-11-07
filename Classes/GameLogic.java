834daf41e9136ac3244bd4124efb2bcef24789e5
biggodrew <andreikwkw@gmail.com>
2025-11-07 17:45:08 -0300

    Create GameLogic.java

---
+++ Classes/GameLogic.java
@@ -0,0 +1,29 @@
+/*
+Função:
+Guarda a posição de cada entidade, as altera com base na 
+direção que o jogador selecionar. Aplica efeitos as entidades
+
+ Atributos:
+- EntidadeJogo posicao[x]{y}: Representa uma Matriz LxL 
+com a posição de cada entidade no grid .
+- Tamanho do grid L
+
+Métodos:
+-Método para alterar a posição de um elemento no grid 
+-Método que retorna a entidade em determinada posição do vetor
+-Método que gerencia uma interação entre duas entidades.
+(ex: Jogador X Inimigo | Atualiza a vida de Ambos com base no poder
+de ataque deles e verifica se estão vivos após a interação)
+-Método que gere aleatoriamente novas entidades quando uma for
+consumida/derrotada.
+
+ */
+package Candyknight;
+
+/**
+ *
+ * @author andre
+ */
+public class GameLogic {
+    
+}
