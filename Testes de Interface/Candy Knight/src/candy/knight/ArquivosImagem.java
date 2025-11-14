95dd0c106fda4a5ee13a51b5ce90f0adebb3f871
Lucas Figueiredo <lucasricardolf11@gmail.com>
2025-11-14 11:13:38 -0300

    Update "ArquivosImagem"

--- Testes de Interface/Candy Knight/src/candy/knight/ArquivosImagem.java
+++ Testes de Interface/Candy Knight/src/candy/knight/ArquivosImagem.java
@@ -1,8 +1,31 @@
 
 package candy.knight;
 
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class ArquivosImagem {
-    public String cavaleiro = "/Imagens/player.png";
-    public String ursoDeGoma = "/Imagens/ursoDeGoma.png";
     
+    public Image cavaleiro(){
+        return this.getImage("/Imagens/player.png");
+    }
+    public Image ursoDeGoma(){
+        return this.getImage("/Imagens/ursoDeGoma.png");
+    }
+    
+    
+    public Image getImage(String nomeDaImagem){
+        Image imagem;
+        
+        try {
+            imagem = ImageIO.read(getClass().getResource(nomeDaImagem));
+            return imagem;
+        } catch (IOException | IllegalArgumentException e) {
+             // Adicionei IllegalArgumentException pois se a pasta estiver errada, o getResource retorna null
+            System.err.println("Erro ao carregar imagem (verifique se a pasta 'Imagens' está correta em Source Packages): " + e.getMessage());
+        }
+        
+        return null;
+    }
 }
